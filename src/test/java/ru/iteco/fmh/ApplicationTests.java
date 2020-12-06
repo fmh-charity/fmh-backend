286dfc65915ec64596322be6e82e8c0ebdc35d78
oleg <maksimovoleg19@gmail.com>
2020-12-06 16:54:20 +0300

    создание бд: * добавил докер контейнер с postgresql

--- src/test/java/ru/iteco/fmh/ApplicationTests.java
+++ src/test/java/ru/iteco/fmh/ApplicationTests.java
@@ -1,20 +1,29 @@
 package ru.iteco.fmh;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class ApplicationTests {
+
+	@Autowired
+	ApplicationContext context;
 
 	@Test
 	public void contextLoads() {
+		Assert.assertNotNull(context);
 	}
 
 	@Autowired
