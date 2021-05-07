d61e64b7a78b55f3101f2c20bbc8f833a1ff5ff6
Дмитрий <pro100proff@mail.ru>
2021-05-07 11:49:11 +0300

    pmh-36 fix problems after rebase

--- src/main/java/ru/iteco/fmh/dto/AdmissionDto.java
+++ src/main/java/ru/iteco/fmh/dto/AdmissionDto.java
@@ -4,6 +4,7 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Data;
+import ru.iteco.fmh.dto.patient.PatientDto;
 
 import java.time.LocalDate;
 
