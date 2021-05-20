8634aeeb41db115aff3787c65c603eb69a4761c3
ArkhipovMarat <maratkzn2010@yandex.ru>
2021-05-20 14:28:41 +0300

    добавил метод addComment + тесты на метод (создал NoteControllerTest.java, NoteServiceTest)

---
+++ src/test/java/ru/iteco/fmh/service/NoteServiceTest.java
@@ -0,0 +1,44 @@
+package ru.iteco.fmh.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.iteco.fmh.dao.repository.NoteRepository;
+import ru.iteco.fmh.dto.note.NoteDto;
+import ru.iteco.fmh.model.Note;
+import ru.iteco.fmh.service.note.NoteService;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static ru.iteco.fmh.TestUtils.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NoteServiceTest {
+    @Autowired
+    NoteService sut;
+
+    @MockBean
+    NoteRepository noteRepository;
+
+    @Test
+    public void addCommentShouldPassSuccess() {
+        // given
+        int noteId = 1;
+        Note note = getNote();
+        String previousComment = note.getComment();
+        String newComment = "test comment";
+        String expected = previousComment.concat(", ").concat(newComment);
+
+        when(noteRepository.findById(any())).thenReturn(Optional.of(note));
+
+        NoteDto result = sut.addComment(noteId, newComment);
+
+        assertEquals(expected, result.getComment());
+    }
+}
