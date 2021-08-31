238aa105f07dbf5cab7cc17992e60e86dacbce67
Anastasiya Sharkouskaya <a.sharkovskaya@yandex.ru>
2021-08-31 15:12:56 +0200

    add ClaimCommentRequestDto.java ClaimCommentToClaimCommentRequestDtoConverter.java ClaimCommentRequestDtoToClaimCommentConverter.java

---
+++ src/main/java/ru/iteco/fmh/converter/claim/fromClaim/ClaimCommentToClaimCommentRequestDtoConverter.java
@@ -0,0 +1,28 @@
+package ru.iteco.fmh.converter.claim.fromClaim;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import ru.iteco.fmh.dto.claim.ClaimCommentRequestDto;
+import ru.iteco.fmh.model.task.claim.ClaimComment;
+
+@Component
+@RequiredArgsConstructor
+public class ClaimCommentToClaimCommentRequestDtoConverter implements Converter<ClaimComment, ClaimCommentRequestDto> {
+
+    @Override
+    public ClaimCommentRequestDto convert(@NonNull ClaimComment claimComment) {
+        ClaimCommentRequestDto dto = new ClaimCommentRequestDto();
+        BeanUtils.copyProperties(claimComment, dto);
+
+        Integer claimId = claimComment.getClaim() != null ? claimComment.getClaim().getId() : null;
+        Integer creatorId = claimComment.getCreator() != null ? claimComment.getCreator().getId() : null;
+
+        dto.setClaimId(claimId);
+        dto.setCreatorId(creatorId);
+
+        return dto;
+    }
+}
---
+++ src/main/java/ru/iteco/fmh/converter/claim/fromDto/ClaimCommentRequestDtoToClaimCommentConverter.java
@@ -0,0 +1,36 @@
+package ru.iteco.fmh.converter.claim.fromDto;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import ru.iteco.fmh.dao.repository.ClaimRepository;
+import ru.iteco.fmh.dao.repository.UserRepository;
+import ru.iteco.fmh.dto.claim.ClaimCommentDto;
+import ru.iteco.fmh.dto.claim.ClaimCommentRequestDto;
+import ru.iteco.fmh.dto.claim.ClaimRequestDto;
+import ru.iteco.fmh.model.task.claim.Claim;
+import ru.iteco.fmh.model.task.claim.ClaimComment;
+import ru.iteco.fmh.model.user.User;
+
+@Component
+@RequiredArgsConstructor
+public class ClaimCommentRequestDtoToClaimCommentConverter implements Converter<ClaimCommentRequestDto, ClaimComment> {
+    private final UserRepository userRepository;
+    private final ClaimRepository claimRepository;
+
+    @Override
+    public ClaimComment convert(@NonNull ClaimCommentRequestDto claimCommentRequestDto) {
+        ClaimComment claimComment = new ClaimComment();
+        BeanUtils.copyProperties(claimCommentRequestDto, claimComment);
+
+        Claim claim = claimCommentRequestDto.getClaimId() != null ?
+                claimRepository.findClaimById(claimCommentRequestDto.getClaimId()) : null;
+        User creator = claimCommentRequestDto.getCreatorId() != null ?
+                userRepository.findUserById(claimCommentRequestDto.getCreatorId()) : null;
+        claimComment.setClaim(claim);
+        claimComment.setCreator(creator);
+        return claimComment;
+    }
+}
