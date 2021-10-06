422188230bdfede4af464a7d5557d6ab60ca2444
Sergey Serdyuk <sergey.serdyuk@agoraworld.co>
2021-10-06 23:40:06 +0300

    Remove unused RequiredArgsConstructor

--- src/main/java/ru/iteco/fmh/controller/UsersController.java
+++ src/main/java/ru/iteco/fmh/controller/UsersController.java
@@ -3,15 +3,7 @@ package ru.iteco.fmh.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.iteco.fmh.dto.user.UserDto;
 import ru.iteco.fmh.dto.user.UserShortInfoDto;
 
@@ -22,7 +14,6 @@ import java.util.List;
  */
 @Api(description = "Информация по пользователю")
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/users")
 public class UsersController {
 
