4c8a14d5b8cb1b9e8b888b772e233ac0e73342b6
Sergey Serdyuk <sergey.serdyuk@agoraworld.co>
2021-10-08 23:31:33 +0300

    Remove unused RequiredArgsConstructor

--- src/main/java/ru/iteco/fmh/controller/ClaimsController.java
+++ src/main/java/ru/iteco/fmh/controller/ClaimsController.java
@@ -4,14 +4,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.iteco.fmh.dto.claim.ClaimCommentDto;
 import ru.iteco.fmh.dto.claim.ClaimDto;
 import ru.iteco.fmh.dto.user.UserDto;
@@ -20,7 +13,7 @@ import ru.iteco.fmh.service.claim.ClaimService;
 
 import java.util.List;
 
-@Api(description = "Заявки")
+@Api("Заявки")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/claims")
