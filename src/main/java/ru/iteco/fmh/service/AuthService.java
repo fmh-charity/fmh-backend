de8743824a6f44c986837b6653e4aa8d3066dd19
Anastasiya Sharkouskaya <a.sharkovskaya@yandex.ru>
2021-10-27 15:50:43 +0200

    fix  Metod AuthService.java

--- src/main/java/ru/iteco/fmh/service/AuthService.java
+++ src/main/java/ru/iteco/fmh/service/AuthService.java
@@ -1,11 +1,11 @@
 package ru.iteco.fmh.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.stereotype.Service;
 import ru.iteco.fmh.dao.repository.TokenRepository;
 import ru.iteco.fmh.dao.repository.UserRepository;
 import ru.iteco.fmh.model.Token;
+import ru.iteco.fmh.model.user.User;
 import ru.iteco.fmh.security.JwtProvider;
 import ru.iteco.fmh.security.JwtResponse;
 import ru.iteco.fmh.security.LoginRequest;
@@ -15,20 +15,18 @@ import java.time.Instant;
 @Service
 @RequiredArgsConstructor
 public class AuthService {
-    private final AuthenticationManager authenticationManager;
     private final JwtProvider jwtProvider;
     private final UserRepository userRepository;
     private final TokenRepository tokenRepository;
 
     public JwtResponse authenticateUser(LoginRequest loginRequest) {
-        //convert user
+        User user = userRepository.findUserByLogin(loginRequest.getLogin());
 
         //get token
-        String accessJwtToken = jwtProvider.generateAccessJwtToken(userDetails);
-        String refreshJwtToken = jwtProvider.generateRefreshJwtToken(userDetails);
+        String accessJwtToken = jwtProvider.generateAccessJwtToken(user);
+        String refreshJwtToken = jwtProvider.generateRefreshJwtToken(user);
 
         Token newToken = Token.builder()
-                .user(userRepository.findUserById(userDetails.getId()))
                 .refreshToken(refreshJwtToken)
                 .createDate(Instant.now())
                 .deleted(false)
