43a40aa92bd78f34f93222baa217dab64064436c
out-nesterenko-do <Nesterenko.D.O@sberbank.ru>
2021-12-09 20:56:37 +0300

    disable cors

--- src/main/java/ru/iteco/fmh/security/WebSecurityConfig.java
+++ src/main/java/ru/iteco/fmh/security/WebSecurityConfig.java
@@ -18,6 +18,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import ru.iteco.fmh.dao.repository.UserRepository;
 
 import java.util.List;
@@ -90,6 +92,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         source.registerCorsConfiguration("/**", configuration);
 
         return source;
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 
     @Bean
