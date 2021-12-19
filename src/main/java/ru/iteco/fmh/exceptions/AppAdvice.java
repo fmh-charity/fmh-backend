2e560734a8f78bd0be5aa06cfe2e9a75c22c1054
out-nesterenko-do <Nesterenko.D.O@sberbank.ru>
2021-12-19 21:15:04 +0300

    fix

--- src/main/java/ru/iteco/fmh/exceptions/AppAdvice.java
+++ src/main/java/ru/iteco/fmh/exceptions/AppAdvice.java
@@ -5,11 +5,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice
-public class AppAdvice {
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Response> handleIllegalArgumentException(Exception e) {
-        Response response = new Response(e.getMessage());
-        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-}
+//@ControllerAdvice
+//public class AppAdvice {
+//    @ExceptionHandler(Exception.class)
+//    public ResponseEntity<Response> handleIllegalArgumentException(Exception e) {
+//        Response response = new Response(e.getMessage());
+//        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+//    }
+//}
