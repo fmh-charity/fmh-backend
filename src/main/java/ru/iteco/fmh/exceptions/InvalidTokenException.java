7fec95d1e83a9aa9cdeff24347ba4b8cb0e4321c
out-nesterenko-do <Nesterenko.D.O@sberbank.ru>
2021-12-19 20:58:39 +0300

    fix response

--- src/main/java/ru/iteco/fmh/exceptions/InvalidTokenException.java
+++ src/main/java/ru/iteco/fmh/exceptions/InvalidTokenException.java
@@ -3,7 +3,7 @@ package ru.iteco.fmh.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.UNAUTHORIZED)
+@ResponseStatus(code = HttpStatus.UNAUTHORIZED, value = HttpStatus.UNAUTHORIZED)
 public class InvalidTokenException extends RuntimeException {
     public InvalidTokenException() {
         super("ERR_INVALID_REFRESH");
