7d94f68d3c00209aa264edc795d9ba384af5a1c8
ilongross <ilongross.dev@gmail.com>
2022-04-20 21:20:21 +0300

    Modules: backend & cucumber-tests

--- src/main/java/ru/iteco/fmh/dao/repository/AdvertisementRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.iteco.fmh.dao.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.iteco.fmh.model.news.News;
-
-@Repository
-public interface AdvertisementRepository extends JpaRepository<News, Integer> {
-}
