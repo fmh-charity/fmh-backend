9a1ae0c02e1b052102ff5d117f73234b8e689c3e
olegkhmil <dactiloscopist@gmail.com>
2022-05-09 11:49:22 +0300

    fmh_back3 после мержа

--- backend/src/main/java/ru/iteco/fmh/controller/BlockController.java
+++ backend/src/main/java/ru/iteco/fmh/controller/BlockController.java
@@ -5,7 +5,14 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import ru.iteco.fmh.dto.block.BlockDto;
 import ru.iteco.fmh.service.block.BlockService;
 
