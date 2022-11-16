64447f9176555ea5ee5a6325ab5cc6d4acd2452b
Vladimir Durov <Shinjitsu0@yandex.ru>
2022-11-16 18:04:02 +0300

    redesigned the web security class

--- backend/src/main/java/ru/iteco/fmh/security/WebSecurityConfig.java
+++ backend/src/main/java/ru/iteco/fmh/security/WebSecurityConfig.java
@@ -1,79 +1,31 @@
 package ru.iteco.fmh.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.CorsUtils;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import ru.iteco.fmh.dao.repository.UserRepository;
-
-import java.util.List;
 
 @Configuration
-@EnableWebSecurity //позволяет Spring находить и автоматически применять класс к глобальной веб-безопасности.
-@EnableConfigurationProperties// Аннотация @EnableConfigurationProperties указывает, что класс будет содержать
-// в качестве особого конфигурационного бина. Not sure that it needs, but this was in example
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+@EnableConfigurationProperties
+@RequiredArgsConstructor
+public class WebSecurityConfig {
 
-
-    UserDetailsServiceImpl userDetailsServiceImpl;
-    UserRepository userRepository;
     private final AuthEntryPointJwt unauthorizedHandler;
 
-
-    public WebSecurityConfig(UserDetailsServiceImpl userDetailsServiceImpl, UserRepository userRepository,
-                             AuthEntryPointJwt unauthorizedHandler) {
-        this.userDetailsServiceImpl = userDetailsServiceImpl;
-        this.userRepository = userRepository;
-        this.unauthorizedHandler = unauthorizedHandler;
-    }
-
-    //https://www.baeldung.com/spring-security-registration-password-encoding-bcrypt
     @Bean
-    public DaoAuthenticationProvider authProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsServiceImpl);
-        authProvider.setPasswordEncoder(passwordEncoder());
-        return authProvider;
-    }
-
-    @Autowired
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authProvider());
-    }
-
-
-    @Override
-    public void configure(WebSecurity web) {
-        // Allow swagger to be accessed without authentication
-        web.ignoring().antMatchers("/v2/api-docs/**")//
-                .antMatchers("/swagger-resources/**")//
-                .antMatchers("/swagger-ui.html")//
-                .antMatchers("/configuration/**")//
-                .antMatchers("/webjars/**")//
-                .antMatchers("/public")
-                .antMatchers("/swagger-ui/**");
-    }
-
-    //https://www.baeldung.com/spring-security-login#3-configuration-for-form-login
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.exceptionHandling().accessDeniedHandler(new SecurityExceptionHandler());
 
         http.cors().and().csrf().disable()
@@ -87,6 +39,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/v2/api-docs/**")//
+                .antMatchers("/swagger-resources/**")//
+                .antMatchers("/swagger-ui.html")//
+                .antMatchers("/configuration/**")//
+                .antMatchers("/webjars/**")//
+                .antMatchers("/public")
+                .antMatchers("/swagger-ui/**");
     }
 
     @Bean
@@ -108,12 +72,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public JwtAuthTokenFilter authenticationJwtTokenFilter() {
         return new JwtAuthTokenFilter();
     }
-
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-
-
 }
