5fc33152aaa8a74a478cc5472274f1fa4d43bcd0
Dmitriy Petrov <nevermind1979@mail.ru>
2022-11-20 21:12:50 +0300

    add WishCommentInfoDto

---
+++ backend/src/main/java/ru/iteco/fmh/dto/wish/WishCommentInfoDto.java
@@ -0,0 +1,27 @@
+package ru.iteco.fmh.dto.wish;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.iteco.fmh.dto.user.UserDtoIdFio;
+
+@ApiModel(description = "информация о комментарии к заявке")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class WishCommentInfoDto {
+    @ApiModelProperty("создатель комментария к просьбе")
+    private UserDtoIdFio userDtoIdFio;
+    @ApiModelProperty("время создания комментария к просьбе")
+    private Long createTime;
+    @ApiModelProperty("текст комментария к просьбе")
+    private String description;
+    @ApiModelProperty("идентификатор комментария к просьбе")
+    private Integer id;
+
+
+}
