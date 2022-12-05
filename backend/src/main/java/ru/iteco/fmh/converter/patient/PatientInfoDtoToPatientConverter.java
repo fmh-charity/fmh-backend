8c0318722fbbf1d2c43f57c127ce52c8b46e7bbd
VlNikoAd <nikolaenko_vladislav@icloud.com>
2022-12-05 10:38:33 +0300

    created my converters, and brought back old converters

--- backend/src/main/java/ru/iteco/fmh/converter/patient/PatientInfoDtoToPatientConverter.java
+++ backend/src/main/java/ru/iteco/fmh/converter/patient/PatientInfoDtoToPatientConverter.java
@@ -5,19 +5,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeanUtils;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
-import ru.iteco.fmh.dto.patient.PatientDto;
+import ru.iteco.fmh.dto.patient.PatientInfoDto;
 import ru.iteco.fmh.model.Patient;
 
 
 /**
- * конвертер из {@link PatientDto} в {@link Patient}
+ * конвертер из {@link PatientInfoDto} в {@link Patient}
  */
 @Component
 @RequiredArgsConstructor
-public class PatientDtoToPatientConverter implements Converter<PatientDto, Patient> {
+public class PatientInfoDtoToPatientConverter implements Converter<PatientInfoDto, Patient> {
 
     @Override
-    public Patient convert(@NonNull PatientDto dto) {
+    public Patient convert(@NonNull PatientInfoDto dto) {
         Patient entity = new Patient();
         BeanUtils.copyProperties(dto, entity);
 
--- backend/src/main/java/ru/iteco/fmh/converter/patient/PatientToPatientDtoConverter.java
+++ backend/src/main/java/ru/iteco/fmh/converter/patient/PatientToPatientDtoConverter.java
@@ -3,18 +3,18 @@ package ru.iteco.fmh.converter.patient;
 import org.springframework.beans.BeanUtils;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
-import ru.iteco.fmh.dto.patient.PatientInfoDto;
+import ru.iteco.fmh.dto.patient.PatientDto;
 import ru.iteco.fmh.model.Patient;
 
 /**
- * конвертер из {@link Patient} в {@link PatientInfoDto}//для «Пациенты» ( Создание пациента, изменение пациента)
+ * конвертер из {@link Patient} в {@link PatientDto}//для «Пациенты» ( Создание пациента, изменение пациента)
  */
 @Component
-public class PatientToPatientDtoConverter implements Converter<Patient, PatientInfoDto> {
+public class PatientToPatientDtoConverter implements Converter<Patient, PatientDto> {
 
     @Override
-    public PatientInfoDto convert(Patient patient) {
-        PatientInfoDto dto = new PatientInfoDto();
+    public PatientDto convert(Patient patient) {
+        PatientDto dto = new PatientDto();
         BeanUtils.copyProperties(patient, dto);
 
         return dto;
