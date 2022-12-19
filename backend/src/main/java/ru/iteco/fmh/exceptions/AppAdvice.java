09b811ab91c5a14bc886c9b57c34787476408829
Zaur Vagabov <vagabov@iteco-inno.ru>
2022-12-19 10:31:23 +0300

    fmh_back19 добавил Advice на ошибки валидации javax

--- backend/src/main/java/ru/iteco/fmh/exceptions/AppAdvice.java
+++ backend/src/main/java/ru/iteco/fmh/exceptions/AppAdvice.java
@@ -5,13 +5,20 @@ import lombok.Getter;
 import lombok.Setter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import ru.iteco.fmh.service.AuthService;
 
+import javax.validation.ConstraintViolationException;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static ru.iteco.fmh.exceptions.ErrorCodes.ERR_INVALID_LOGIN;
 import static ru.iteco.fmh.exceptions.ErrorCodes.ERR_INVALID_REFRESH;
@@ -40,11 +47,37 @@ public class AppAdvice {
                 .body(ErrorResponse.builder().code(errorCode).message(e.getMessage()).build());
     }
 
-    @Builder
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ValidationErrorResponse onConstraintValidationException(ConstraintViolationException e) {
+        final List<Violation> violations = e.getConstraintViolations().stream()
+                .map(violation -> new Violation(violation.getPropertyPath().toString(), violation.getMessage()))
+                .collect(Collectors.toList());
+        return new ValidationErrorResponse(violations);
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ValidationErrorResponse onMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        final List<Violation> violations = e.getBindingResult().getFieldErrors().stream()
+                .map(error -> new Violation(error.getField(), error.getDefaultMessage()))
+                .collect(Collectors.toList());
+        return new ValidationErrorResponse(violations);
+    }
+
     @Getter
     @Setter
+    @Builder
     private static class ErrorResponse {
         private ErrorCodes code;
         private String message;
+    }
+
+    public record ValidationErrorResponse(List<Violation> violations) {
+    }
+
+    public record Violation(String fieldName, String message) {
     }
 }
