e435b4565e7b870a0e2cc379f6f8e78a8d27ca85
Petrov Dmitriy <dmitr.petrov.spb@gmail.com>
2023-01-06 17:59:48 +0300

    add Security

--- backend/src/main/java/ru/iteco/fmh/controller/DocumentsController.java
+++ backend/src/main/java/ru/iteco/fmh/controller/DocumentsController.java
@@ -6,6 +6,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,6 +34,7 @@ public class DocumentsController {
     }
     private final DocumentService documentService;
 
+    @Secured("ROLE_ADMINISTRATOR")
     @Operation(summary = "Создание документа")
     @PostMapping
     public DocumentCreationDtoRs createDocument(@RequestBody @Valid DocumentCreationDtoRq documentCreationDtoRqq) {
