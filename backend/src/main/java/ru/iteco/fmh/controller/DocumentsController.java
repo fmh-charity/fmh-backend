8ec95b1e4946b2d36663b04fdb8cd31093597855
Vladimir Durov <Shinjitsu0@yandex.ru>
2023-01-13 18:44:23 +0300

    fmh_back31 fixed checkstyle warnings

--- backend/src/main/java/ru/iteco/fmh/controller/DocumentsController.java
+++ backend/src/main/java/ru/iteco/fmh/controller/DocumentsController.java
@@ -6,7 +6,12 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.multipart.MultipartFile;
 import ru.iteco.fmh.service.document.DocumentService;
 
