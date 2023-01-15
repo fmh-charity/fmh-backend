2740442a7a8f2506dff9bd5e0f651b17a71f9ea0
Petrov Dmitriy <dmitr.petrov.spb@gmail.com>
2023-01-15 10:51:10 +0300

    fmh_back29 update yml files and update converter

--- backend/src/main/java/ru/iteco/fmh/converter/document/DocumentToDocumentCreationDtoRsConverter.java
+++ backend/src/main/java/ru/iteco/fmh/converter/document/DocumentToDocumentCreationDtoRsConverter.java
@@ -1,6 +1,7 @@
 package ru.iteco.fmh.converter.document;
 
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
@@ -9,6 +10,8 @@ import ru.iteco.fmh.dto.document.DocumentCreationDtoRs;
 import ru.iteco.fmh.dto.user.UserDtoIdFio;
 import ru.iteco.fmh.model.document.Document;
 
+import java.net.URI;
+
 @Component
 @RequiredArgsConstructor
 public class DocumentToDocumentCreationDtoRsConverter implements Converter<Document, DocumentCreationDtoRs> {
@@ -16,13 +19,14 @@ public class DocumentToDocumentCreationDtoRsConverter implements Converter<Docum
     @Value("${static-host.host}")
     private String staticHost;
 
+    @SneakyThrows
     @Override
     public DocumentCreationDtoRs convert(Document source) {
         UserDtoIdFio userDtoIdFio = conversionService.convert(source.getUser());
         return DocumentCreationDtoRs.builder()
                 .id(source.getId())
                 .name(source.getName())
-                .filePath(staticHost + source.getFilePath())
+                .filePath(new URI(staticHost+source.getFilePath()).normalize().toString())
                 .description(source.getDescription())
                 .status(source.getStatus())
                 .createDate(source.getCreateDate())
