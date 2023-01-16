ba335ccdd859f57596be4add9f282d1ab082913a
Petrov Dmitriy <dmitr.petrov.spb@gmail.com>
2023-01-16 21:28:18 +0300

    fmh_back36 fix build errors

--- backend/src/main/java/ru/iteco/fmh/dao/repository/DocumentRepository.java
+++ backend/src/main/java/ru/iteco/fmh/dao/repository/DocumentRepository.java
@@ -3,6 +3,7 @@ package ru.iteco.fmh.dao.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.iteco.fmh.model.document.Document;
+
 @Repository
 public interface DocumentRepository extends JpaRepository<Document, Integer> {
 }
