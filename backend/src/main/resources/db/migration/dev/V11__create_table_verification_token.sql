851cfea2ae51c61fb7e0f3123e33f797686657be
Vladimir Durov <Shinjitsu0@yandex.ru>
2023-01-27 11:44:03 +0300

    fmh_back42 wrong migration version 11 was deleted

--- backend/src/main/resources/db/migration/dev/V11__create_table_verification_token.sql
+++
@@ -1,7 +0,0 @@
-create table verification_token
-(
-    id          serial primary key,
-    user_id     int references users not null,
-    token       varchar(64) not null,
-    expiry_date timestamp not null
-);
