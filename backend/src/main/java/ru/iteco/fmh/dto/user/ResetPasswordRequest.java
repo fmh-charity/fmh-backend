24e910b1a1d3d9eebd1aecf43a3b49b2df6ee41d
Petrov Dmitriy <dmitr.petrov.spb@gmail.com>
2023-01-29 15:44:21 +0300

    fmh_back40 add ResetPasswordRequest

---
+++ backend/src/main/java/ru/iteco/fmh/dto/user/ResetPasswordRequest.java
@@ -0,0 +1,26 @@
+package ru.iteco.fmh.dto.user;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@Schema(description = "Информация по обновлению пароля")
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = PRIVATE)
+public class ResetPasswordRequest {
+    String login;
+    @Size(min=6)
+    @Pattern(regexp = "[a-zA-Z0-9!#$%&_-]+")
+    String password;
+}
