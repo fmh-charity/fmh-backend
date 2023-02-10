c49bb94031186768a874eb2eaee7609b6c64c9cf
Шурупов Евгений <shurupin@gmail.com>
2023-02-10 21:12:02 +0300

    fmh_back43 Добавить функционал отправки e-mail

---
+++ backend/src/main/java/ru/iteco/fmh/service/mail/notifier/SendEmailNotifierContext.java
@@ -0,0 +1,14 @@
+package ru.iteco.fmh.service.mail.notifier;
+
+import lombok.Data;
+
+@Data
+public class SendEmailNotifierContext extends NotifierAbstractContext {
+
+    private String toAddress; //кому
+    private String fromAddress; //от кого
+    private String senderName; //организация отправитель
+    private String subject; //тема письма
+    private String content; //текст письма в формате html
+
+}
